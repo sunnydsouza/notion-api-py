8198b36cd28b0fc1306fe3eabcc8c432fbc6b74b
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-04-08 19:41:55 +0530

    test(notion_api): Just added skeleton scenarios to cover in future.

--- tests/test_notion_api_py.py
+++ tests/test_notion_api_py.py
@@ -3,3 +3,90 @@ from notion_api_py import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_sample_useless_test():
+    assert 1 == 1
+
+
+def test_db_add_page():
+    assert 1 == 1
+
+
+def test_db_update_page():
+    assert 1 == 1
+
+
+def test_db_delete_page():
+    assert 1 == 1
+
+
+def test_db_filter():
+    assert 1 == 1
+
+
+def test_properties_get_json_string():
+    assert 1 == 1
+
+
+def test_properties_number():
+    assert 1 == 1
+
+
+def test_properties_text():
+    assert 1 == 1
+
+
+def test_properties_title():
+    assert 1 == 1
+
+
+def test_properties_date():
+    assert 1 == 1
+
+
+def test_properties_checkbox():
+    assert 1 == 1
+
+
+def test_properties_select():
+    assert 1 == 1
+
+
+def test_properties_multi_select():
+    assert 1 == 1
+
+
+def test_properties_relation():
+    assert 1 == 1
+
+
+def test_relations_overwrite():
+    assert 1 == 1
+
+
+def test_relations_append():
+    assert 1 == 1
+
+
+# def test_relations_remove():
+#     assert 1 == 1
+
+def test_filters_compond_and():
+    assert 1 == 1
+
+
+def test_filters_compond_or():
+    assert 1 == 1
+
+
+def test_filters_simple_relationfilter():
+    assert 1 == 1
+
+
+def test_filters_simple_checkboxfilter():
+    assert 1 == 1
+
+
+def test_filters_simple_textfilter():
+    assert 1 == 1
+
