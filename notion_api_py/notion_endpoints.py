a1ec4cccdd060208b6319b8cb2ee16e44a8db383
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-04-08 19:41:23 +0530

    chore(notion_api): Segregated notion api endpoint urls

---
+++ notion_api_py/notion_endpoints.py
@@ -0,0 +1,9 @@
+update_retrieve_database = "https://api.notion.com/v1/databases/:id"
+query_database = "https://api.notion.com/v1/databases/:id/query"
+create_database = "https://api.notion.com/v1/databases/"
+
+create_page = "https://api.notion.com/v1/pages/"
+update_retrieve_page_properties = "https://api.notion.com/v1/pages/:id"
+
+retrieve_append_block_children = "https://api.notion.com/v1/blocks/:id/children"
+update_retrieve_block = "https://api.notion.com/v1/blocks/:id"
